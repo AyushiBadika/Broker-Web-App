6de9e70f11bf0969ecebaf18a54d9f534c2d38b4
AyushiBadika <badikaayushi@gmail.com>
2024-10-14 12:22:46 +0530

    created header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to={"/"}>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-primaryText ">Real</span>
+            <span className="text-SecondaryText ">State</span>
+          </h1>
+        </Link>
+        <form className="flex items-center bg-slate-100 p-3 rounded-lg">
+          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <div className="flex gap-8 font-semibold">
+          <Link to={"/"} className="hover:underline">
+            Home
+          </Link>
+          <Link to="/about" className="hover:underline">
+            About
+          </Link>
+          <Link to="/sign-in" className="hover:underline">
+            Sign In
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primaryBg: "bg-slate-500",
+        // SecondaryBg: "bg-slate-700",
+        primaryText: "#65758B",
+        SecondaryText: "#324055",
+      },
+    },
   },
   plugins: [],
 };
