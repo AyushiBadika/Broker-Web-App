74427fd2c35ed7f57c6fa9bac01f651b22ed662e
AyushiBadika <badikaayushi@gmail.com>
2024-10-14 13:11:15 +0530

    create and run server and connect to database

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.log(err));
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log("Server is running on port 3000!!!");
+});
