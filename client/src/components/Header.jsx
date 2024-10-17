616563b36652f22c42eac6cb76bd187c76af8756
AyushiBadika <badikaayushi@gmail.com>
2024-10-18 00:34:18 +0530

    complete image upload functionality using firebase

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -21,8 +23,8 @@ export default function Header() {
           <Link to="/about" className="hover:underline">
             About
           </Link>
-          <Link to="/sign-in" className="hover:underline">
-            Sign In
+          <Link to="/profile" className="hover:underline">
+            {currentUser ? <img src={currentUser.avatar} alt="profile" className="rounded-full w-7 h-7 object-cover" /> : "Sign In"}
           </Link>
         </div>
       </div>
