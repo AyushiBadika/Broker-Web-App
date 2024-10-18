9ba64d2e820212a091e1ea37907347efd061958b
AyushiBadika <badikaayushi@gmail.com>
2024-10-18 20:45:46 +0530

    fix error

--- api/index.js
+++ api/index.js
@@ -26,7 +26,6 @@ app.use("/api/listing", listingRoutes);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(__dirname);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
