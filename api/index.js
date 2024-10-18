ae39b2be3e5245736607d875bd09db791ea6d782
AyushiBadika <badikaayushi@gmail.com>
2024-10-18 19:24:44 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import listingRoutes from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 mongoose
   .connect(process.env.CONNECTION_STRING)
@@ -21,6 +22,12 @@ const port = process.env.PORT || 3001;
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/listing", listingRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
