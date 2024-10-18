e740d7d4c43ce501d40081cf01af63cf3320a0dd
AyushiBadika <badikaayushi@gmail.com>
2024-10-18 15:29:17 +0530

    create update listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import ListingModel from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -14,6 +15,42 @@ export const getUserListing = async (req, res, next) => {
     const listings = await ListingModel.find({ userRef: req.user.id });
 
     res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteListing = async (req, res, next) => {
+  const listing = await ListingModel.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler({ statusCode: 404, message: "Listing not found!" }));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler({ statusCode: 401, message: "You can only delete your own listings" }));
+  }
+  try {
+    const listings = await ListingModel.findOneAndDelete({ userRef: req.user.id });
+
+    res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+export const updateListing = async (req, res, next) => {
+  const listing = await ListingModel.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler({ statusCode: 404, message: "Listing not found!" }));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler({ statusCode: 401, message: "You can only update your own listings" }));
+  }
+  try {
+    const updatedListing = await ListingModel.findOneAndUpdate(req.params.id, req.body, { new: true });
+
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createListing, getUserListing } from "../controllers/listing.controller.js";
+import { createListing, getUserListing, deleteListing, updateListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.get("/listings", verifyToken, getUserListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
